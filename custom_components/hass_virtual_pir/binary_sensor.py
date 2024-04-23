c8fa94dd03aa07109e80ce2dd559c919803c1c75
anasnajj <anasnajj@gmail.com>
2024-04-24 01:27:11 +0300

    Update binary_sensor.py

--- custom_components/hass_virtual_pir/binary_sensor.py
+++ custom_components/hass_virtual_pir/binary_sensor.py
@@ -1,37 +1,29 @@
 import asyncio
-import subprocess
-from homeassistant.helpers.entity import ToggleEntity
-from homeassistant.const import CONF_IP_ADDRESS
+from homeassistant.helpers.entity import Entity
+from homeassistant.const import STATE_CLEAR, STATE_DETECTED
+from subprocess import DEVNULL, PIPE, run
 
 DOMAIN = "hass_virtual_pir"
 
-async def async_setup_entry(hass, config_entry, async_add_entities):
-    ip_address = config_entry.data.get(CONF_IP_ADDRESS)
-    async_add_entities([VirtualPIRSensor(ip_address)])
+async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
+    async_add_entities([VirtualPIRSensor()])
 
-class VirtualPIRSensor(ToggleEntity):
-    def __init__(self, ip_address):
-        self._ip_address = ip_address
-        self._state = None
+class VirtualPIRSensor(Entity):
+    def __init__(self):
+        self._state = STATE_CLEAR
 
     @property
     def name(self):
         return "Virtual PIR Sensor"
 
     @property
-    def is_on(self):
-        return self._state == "Detected"
+    def state(self):
+        return self._state
 
     async def async_update(self):
-        result = await self._ping_ip()
-        self._state = "Detected" if result else "Clear"
-
-    async def _ping_ip(self):
-        try:
-            await asyncio.wait_for(
-                subprocess.run(["ping", "-c", "1", self._ip_address], capture_output=True),
-                timeout=5
-            )
-            return True
-        except asyncio.TimeoutError:
-            return False
+        # Replace "127.0.0.1" with the IP address you want to ping
+        result = await self.hass.async_add_executor_job(run, ["ping", "-c", "1", "-W", "1", "127.0.0.1"], stdout=DEVNULL, stderr=PIPE)
+        if result.returncode == 0:
+            self._state = STATE_DETECTED
+        else:
+            self._state = STATE_CLEAR
