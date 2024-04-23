27d4f8966ab5dcee2d4449d172c8c8944062d411
anasnajj <anasnajj@gmail.com>
2024-04-24 01:33:36 +0300

    Update config_flow.py

--- custom_components/hass_virtual_pir/config_flow.py
+++ custom_components/hass_virtual_pir/config_flow.py
@@ -1,29 +1,31 @@
 import voluptuous as vol
 from homeassistant import config_entries
 from subprocess import run, PIPE
+
 DOMAIN = "hass_virtual_pir"
 
-class VirtualPIRFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
+class VirtualPIRFlowHandler(config_entries.ConfigFlow):
 
     async def async_step_user(self, user_input=None):
+        errors = {}
+
         if user_input is not None:
             # Validate the entered IP address
-            ip_address = user_input["ip_address"]
-            if not await self.hass.async_add_executor_job(self._ping_ip, ip_address):
-                return self.async_show_form(
-                    step_id="user",
-                    errors={"base": "Failed to ping the specified IP address. Please enter a valid IP."}
-                )
-
-            # IP address is valid, proceed with configuration
-            return self.async_create_entry(title="Virtual PIR Configuration", data=user_input)
+            ip_address = user_input.get("ip_address")
+            if not ip_address:
+                errors["base"] = "Please enter an IP address."
+            elif not await self.hass.async_add_executor_job(self._ping_ip, ip_address):
+                errors["base"] = "Failed to ping the specified IP address. Please enter a valid IP."
+            else:
+                return self.async_create_entry(title="Virtual PIR Configuration", data=user_input)
 
         # Show form to enter IP address
         return self.async_show_form(
             step_id="user",
             data_schema=vol.Schema({
                 vol.Required("ip_address"): str
-            })
+            }),
+            errors=errors
         )
 
     def _ping_ip(self, ip_address):
