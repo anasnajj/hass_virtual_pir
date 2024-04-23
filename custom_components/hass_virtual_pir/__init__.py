5ecac5fa266757b33d1a8a7d2941698e2cbd94da
anasnajj <anasnajj@gmail.com>
2024-04-24 02:16:54 +0300

    Update __init__.py

--- custom_components/hass_virtual_pir/__init__.py
+++ custom_components/hass_virtual_pir/__init__.py
@@ -1,7 +1,27 @@
-"""The Virtual PIR Sensor integration."""
-DOMAIN = "hass_virtual_pir"
-PLATFORMS = ["sensor"]
+import asyncio
+import async_timeout
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
 
-async def async_setup(hass, config):
-    """Set up the Virtual PIR Sensor component."""
+from .const import DOMAIN, CONF_IP_ADDRESS, DEFAULT_TIMEOUT
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up the platform from a config entry."""
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    hass.async_create_task(
+        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
+    )
     return True
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    unload_ok = await hass.config_entries.async_forward_entry_unload(
+        entry, "binary_sensor"
+    )
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
