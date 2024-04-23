fbe1d8794a05056f7826f62e7997efa51e006094
anasnajj <anasnajj@gmail.com>
2024-04-24 02:09:05 +0300

    Update sensor.py

--- custom_components/hass_virtual_pir/sensor.py
+++ custom_components/hass_virtual_pir/sensor.py
@@ -1,31 +1,35 @@
 """Platform for the Virtual PIR Sensor integration."""
 from homeassistant.helpers.entity import Entity
-from homeassistant.const import STATE_UNKNOWN
-from ping3 import ping
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import CONF_IP_ADDRESS
+from homeassistant.core import HomeAssistant
 from datetime import timedelta
+from ping3 import ping
 
 SCAN_INTERVAL = timedelta(seconds=10)
 
-async def async_setup_entry(hass, entry, async_add_entities):
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Virtual PIR Sensor from a config entry."""
-    async_add_entities([VirtualPIRSensor(entry.data)])
+    hass.data.setdefault("virtual_pir", {})
+    hass.data["virtual_pir"][entry.entry_id] = VirtualPIRSensor(entry)
+    return True
 
 class VirtualPIRSensor(Entity):
     """Representation of a Virtual PIR Sensor."""
 
-    def __init__(self, config):
+    def __init__(self, entry: ConfigEntry):
         """Initialize the sensor."""
-        self._name = "Virtual PIR Sensor"
-        self._ip_address = config.get(CONF_IP_ADDRESS)
-        self._state = STATE_UNKNOWN
+        self._entry = entry
+        self._ip_address = entry.data.get(CONF_IP_ADDRESS)
+        self._state = None
 
     @property
-    def name(self):
+    def name(self) -> str:
         """Return the name of the sensor."""
-        return self._name
+        return self._entry.title
 
     @property
-    def state(self):
+    def state(self) -> str:
         """Return the state of the sensor."""
         return self._state
 
@@ -37,4 +41,4 @@ class VirtualPIRSensor(Entity):
             else:
                 self._state = "Clear"
         else:
-            self._state = STATE_UNKNOWN
+            self._state = None
