b996a4fa17fa450f2c6754a3c6ab4e2a65672182
anasnajj <anasnajj@gmail.com>
2024-04-24 01:46:42 +0300

    Update __init__.py

--- custom_components/hass_virtual_pir/__init__.py
+++ custom_components/hass_virtual_pir/__init__.py
@@ -1,5 +1,6 @@
 """The Virtual PIR Sensor integration."""
 DOMAIN = "hass_virtual_pir"
+PLATFORMS = ["sensor"]
 
 async def async_setup(hass, config):
     """Set up the Virtual PIR Sensor component."""
