f239f36c1148c06000049efef09d92639ebdff40
anasnajj <anasnajj@gmail.com>
2024-04-24 04:43:02 +0300

    Update __init__.py

--- custom_components/hass_virtual_pir/__init__.py
+++ custom_components/hass_virtual_pir/__init__.py
@@ -6,28 +6,13 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN, CONF_IP_ADDRESS, DEFAULT_TIMEOUT
 
-PLATFORMS = ["binary_sensor"]  # Include the binary_sensor platform
+PLATFORMS = ["binary_sensor"]  
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up the platform from a config entry."""
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = entry.data
-
-    # Retrieve the IP address entered by the user
-    ip_address = entry.data[CONF_IP_ADDRESS]
+    ip_address = entry.data[CONF_IP_ADDRESS] 
 
     forward_entry_setup = hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
-    async_add_entities: AddEntitiesCallback = forward_entry_setup
+    async_add_entities: AddEntitiesCallback = forward_entry_setup  
 
     async_add_entities([VirtualPIRSensor(ip_address)], True)
     return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(
-        entry, "binary_sensor"
-    )
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id)
-
-    return unload_ok
