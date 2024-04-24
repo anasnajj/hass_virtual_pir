781ba516f054709791c05d47f1409abadf53d95a
anasnajj <anasnajj@gmail.com>
2024-04-24 03:59:43 +0300

    Update const.py

--- custom_components/hass_virtual_pir/const.py
+++ custom_components/hass_virtual_pir/const.py
@@ -1,3 +1,3 @@
 DOMAIN = "hass_virtual_pir"  # The domain of your integration
-CONF_IP_ADDRESS = "ip_address"  # The key used to reference the IP address
+CONF_IP_ADDRESS = "ip_address" # The key used to reference the IP address
 DEFAULT_TIMEOUT = 5  # Default timeout (in seconds) for ping checks
